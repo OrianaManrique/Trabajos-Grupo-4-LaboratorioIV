dbdc43b3422ed133ec11a5c34147bca1a596dfc5
Leoxs122 <leonelcejas09@gmail.com>
2025-04-15 14:37:27 -0300

    Actualización de Persona.java

--- TP3_GRUPO_4/src/Ejercicio1/Persona.java
+++ TP3_GRUPO_4/src/Ejercicio1/Persona.java
@@ -1,5 +1,42 @@
-package Ejercicio1;
-
 public class Persona {
+    private String nombre;
+    private String apellido;
+    private String dni;
 
+    // Constructor
+    public Persona(String nombre, String apellido, String dni) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+    }
+
+    // Getter  para nombre 
+    public String getNombre() {
+        return nombre;
+    }
+
+    // Setter para nombre
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    // Getter para apellido
+    public String getApellido() {
+        return apellido;
+    }
+
+    // Setter para apellido
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    // Getter para dni
+    public String getDni() {
+        return dni;
+    }
+
+    // Setter para dni
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
 }
