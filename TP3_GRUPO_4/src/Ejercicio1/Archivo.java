81cb228ff7423af012b4cdbe22748be27c9392f1
OrianaManrique <orianamanriqueutlook.com.ar>
2025-04-18 22:51:41 -0300

    metodos leer y escribir con buffer

--- TP3_GRUPO_4/src/Ejercicio1/Archivo.java
+++ TP3_GRUPO_4/src/Ejercicio1/Archivo.java
@@ -1,7 +1,11 @@
 package Ejercicio1;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+
 
 public class Archivo {
 	
@@ -36,5 +40,37 @@ public class Archivo {
 
 	public void setRuta(String ruta) {
 		this.ruta = ruta;
+	}
+	
+	public void escribir_lineas (String frase) {
+		try {
+			FileWriter entrada = new FileWriter(ruta, true);
+			BufferedWriter miBuffer = new BufferedWriter(entrada);
+			miBuffer.write(frase);
+			miBuffer.close();
+			entrada.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void leer_lineas() {
+		FileReader entrada;
+		try {
+			entrada = new FileReader(ruta);
+			BufferedReader miBuffer = new BufferedReader(entrada);
+
+		String linea = "";
+			while (linea != null) {
+			System.out.println(linea);
+			linea = miBuffer.readLine();
+			}
+			
+			miBuffer.close();
+			entrada.close();
+
+		} catch (IOException e) {
+			System.out.println("No se encontró el archivo");
+		}
 	}
 }
