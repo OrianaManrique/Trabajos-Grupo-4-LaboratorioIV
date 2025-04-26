c26d33a5b54a2ff7857fa62cc3b7854c62bb053f
BraianFranco <BFraanco66@gmail.com>
2025-04-26 02:58:14 -0300

    Creacion de la clase Ventana

--- TP4_GRUPO_4/src/Ejercicios/Principal.java
+++ TP4_GRUPO_4/src/Ejercicios/Principal.java
@@ -1,20 +1,13 @@
 package Ejercicios;
 
-import javax.swing.JFrame;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		JFrame frame = new JFrame ();
+		Ventana ventana = new Ventana();
+		ventana.cambiarVisibilidad(true);
 		
-		frame.setSize(500,500);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLocation(350,350);
-		frame.setVisible(true);
-	    frame.setTitle("contactos");
-
 	}
 
 }
