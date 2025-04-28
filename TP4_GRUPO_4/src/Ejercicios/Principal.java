a3ba973b10c2bef776c0285272cdd083fd1f3399
OrianaManrique <orianamanriqueutlook.com.ar>
2025-04-28 01:34:46 -0300

    ventana principal y ventanaEjercicio1

--- TP4_GRUPO_4/src/Ejercicios/Principal.java
+++ TP4_GRUPO_4/src/Ejercicios/Principal.java
@@ -1,13 +1,11 @@
 package Ejercicios;
 
-
 public class Principal {
 
-	public static void main(String[] args) {
-		
-		Ventana ventana = new Ventana();
-		ventana.cambiarVisibilidad(true);
-		
-	}
+    public static void main(String[] args) {
 
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.setVisible(true);
+
+    }
 }
---
+++ TP4_GRUPO_4/src/Ejercicios/VentanaEjercicio1.java
@@ -0,0 +1,101 @@
+package Ejercicios;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.JTextField;
+
+public class VentanaEjercicio1 extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField txtNombre;
+	private JTextField txtApellido;
+	private JTextField txtTelefono;
+	private JTextField txtFechaNac;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaEjercicio1 frame = new VentanaEjercicio1();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public VentanaEjercicio1() {
+		setDefaultCloseOperation(VentanaEjercicio1.DISPOSE_ON_CLOSE);
+		setBounds(150, 150, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setTitle("Contactos");
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNombre = new JLabel("Nombre");
+		lblNombre.setFont(new Font("Calibri", Font.BOLD, 11));
+		lblNombre.setBounds(36, 59, 46, 14);
+		contentPane.add(lblNombre);
+		
+		JLabel lblApellido = new JLabel("Apellido");
+		lblApellido.setFont(new Font("Calibri", Font.BOLD, 11));
+		lblApellido.setBounds(36, 84, 46, 14);
+		contentPane.add(lblApellido);
+		
+		JLabel lblTelefono = new JLabel("Teléfono");
+		lblTelefono.setFont(new Font("Calibri", Font.BOLD, 11));
+		lblTelefono.setBounds(36, 109, 46, 14);
+		contentPane.add(lblTelefono);
+		
+		JLabel lblFechaNac = new JLabel("Fecha Nac.");
+		lblFechaNac.setFont(new Font("Calibri", Font.BOLD, 11));
+		lblFechaNac.setBounds(36, 134, 60, 14);
+		contentPane.add(lblFechaNac);
+		
+		JLabel lblDatosIngresados = new JLabel("Los datos ingresados fueron:");
+		lblDatosIngresados.setFont(new Font("Calibri", Font.BOLD, 11));
+		lblDatosIngresados.setBounds(10, 236, 179, 14);
+		lblDatosIngresados.setVisible(false);
+		contentPane.add(lblDatosIngresados);
+		
+		txtNombre = new JTextField();
+		txtNombre.setBounds(163, 54, 86, 20);
+		contentPane.add(txtNombre);
+		txtNombre.setColumns(10);
+		
+		txtApellido = new JTextField();
+		txtApellido.setColumns(10);
+		txtApellido.setBounds(163, 79, 86, 20);
+		contentPane.add(txtApellido);
+		
+		txtTelefono = new JTextField();
+		txtTelefono.setColumns(10);
+		txtTelefono.setBounds(163, 104, 86, 20);
+		contentPane.add(txtTelefono);
+		
+		txtFechaNac = new JTextField();
+		txtFechaNac.setColumns(10);
+		txtFechaNac.setBounds(163, 129, 86, 20);
+		contentPane.add(txtFechaNac);
+		
+		JButton btnMostrar = new JButton("Mostrar");
+		btnMostrar.setBounds(300, 88, 102, 24);
+		contentPane.add(btnMostrar);
+	}
+}
