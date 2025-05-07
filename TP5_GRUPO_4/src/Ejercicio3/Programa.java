1f592314c9d00e8b08805b5d021333cadfbba4f2
Nahuel Ivan <arellano.nahuel95@gmail.com>
2025-05-07 08:38:38 -0300

    agrego jmenubar jmenu y jmenuitems

--- TP5_GRUPO_4/src/Ejercicio3/Programa.java
+++ TP5_GRUPO_4/src/Ejercicio3/Programa.java
@@ -3,6 +3,9 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Color;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class Programa extends JFrame {//CODIGO VENTANA SOLO LO QUE CREAMOS EN VENTANA
 
@@ -24,10 +27,23 @@ public class Programa extends JFrame {//CODIGO VENTANA SOLO LO QUE CREAMOS EN VE
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
-		contentPane.setBackground(Color.GREEN);
+		//contentPane.setBackground(Color.GREEN);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
        
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBounds(0, 0, 434, 22);
+		contentPane.add(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("Peliculas");
+		menuBar.add(mnNewMenu);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("Agregar");
+		mnNewMenu.add(mntmNewMenuItem);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Listar");
+		mnNewMenu.add(mntmNewMenuItem_1);
 	}
 }
