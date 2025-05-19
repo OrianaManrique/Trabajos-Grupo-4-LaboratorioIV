7051641b152dee2071ed527a8f030ce3f6565489
Fabrizio Alejandro De Rui Sartor <fabrizioderuisartor@gmail.com>
2025-05-19 20:11:48 -0300

    Agregando menu persona
    Signed-off-by: Fabrizio Alejandro De Rui Sartor <fabrizioderuisartor@gmail.com>

---
+++ TP6_GRUPO_4/src/main/Principal.java
@@ -0,0 +1,98 @@
+package main;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Principal extends JFrame {
+
+	private JPanel contentPane;
+
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Principal frame = new Principal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	public Principal() {
+
+		setTitle("Programa");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 488, 397);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		menuBar.setBounds(0, 0, 434, 22);
+		
+		JMenu mnNewMenu = new JMenu("Persona");
+		mnNewMenu.setFont(new Font("Trebuchet MS", Font.BOLD, 18));
+		menuBar.add(mnNewMenu);
+		
+		//Boton Agregar
+		JMenuItem menuItemAgregar = new JMenuItem("Agregar");
+		menuItemAgregar.setFont(new Font("Trebuchet MS", Font.BOLD, 18));
+		
+		menuItemAgregar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		mnNewMenu.add(menuItemAgregar);
+		
+		//Boton Modificar
+		JMenuItem menuItemModificar = new JMenuItem("Modificar");
+		menuItemModificar.setFont(new Font("Trebuchet MS", Font.BOLD, 18));
+		
+		menuItemModificar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		mnNewMenu.add(menuItemModificar);
+		
+		//Boton Eliminar
+		JMenuItem menuItemEliminar = new JMenuItem("Eliminar");
+		menuItemEliminar.setFont(new Font("Trebuchet MS", Font.BOLD, 18));
+		
+		menuItemEliminar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		mnNewMenu.add(menuItemEliminar);
+		
+		//Boton Listar
+		JMenuItem menuItemListar = new JMenuItem("Listar");
+		menuItemListar.setFont(new Font("Trebuchet MS", Font.BOLD, 18));
+		
+		menuItemListar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		mnNewMenu.add(menuItemListar);
+		
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
--- TP6_GRUPO_4/src/main/UsuarioDB.java
+++ TP6_GRUPO_4/src/main/UsuarioDB.java
@@ -1,5 +1,48 @@
 package main;
 
 public class UsuarioDB {
+	
+	private int dni;
+	private String nombre;
+	private String apellido;
+	
+	public UsuarioDB() {
+		
+	}
 
+	public UsuarioDB(int dni, String nombre, String apellido) {
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellido = apellido;
+	}
+
+	public int getDni() {
+		return dni;
+	}
+
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	@Override
+	public String toString() {
+		return "UsuarioDB [dni=" + dni + ", nombre=" + nombre + ", apellido=" + apellido + "]";
+	}
+	
 }
