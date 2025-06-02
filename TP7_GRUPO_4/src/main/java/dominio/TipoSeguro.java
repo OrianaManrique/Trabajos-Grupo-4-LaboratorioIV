94d172b23ecc1181387241d700c18c8cd6253570
BraianFranco <BFraanco66@gmail.com>
2025-06-02 16:13:44 -0300

    Paquete con clases

---
+++ TP7_GRUPO_4/src/main/java/dominio/TipoSeguro.java
@@ -0,0 +1,34 @@
+package dominio;
+
+public class TipoSeguro {
+    private int idTipo;
+    private String Descripcion;
+
+    public TipoSeguro() {}
+
+    public TipoSeguro(int idTipo, String Descripcion) {
+        this.idTipo = idTipo;
+        this.Descripcion = Descripcion;
+    }
+
+	public int getIdTipo() {
+		return idTipo;
+	}
+	
+	public void setIdTipo(int idTipo) {
+		this.idTipo = idTipo;
+	}
+
+	public String getDescripcion() {
+		return Descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		Descripcion = descripcion;
+	}
+
+	public String toString() {
+		return "TipoSeguro [idTipo=" + idTipo + ", Descripcion=" + Descripcion + "]";
+	}
+    	
+}
---
+++ TP7_GRUPO_4/src/main/webapp/Inicio.jsp
@@ -0,0 +1,10 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head><title>Inicio</title></head>
+<body>
+    <h2>Soy la página inicio</h2>
+    <a href= "Inicio.jsp">Inicio</a>
+</body>
+</html>
