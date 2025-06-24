b282ea6fdc81347bf0663733874ae0eb8f5c6bac
OrianaManrique <orianamanriqueutlook.com.ar>
2025-06-24 00:04:32 -0300

    clase tipo movimiento

---
+++ TPINT_GRUPO_4_LAB4/src/main/java/entidad/Tipo_Movimientos.java
@@ -0,0 +1,45 @@
+package entidad;
+
+public class Tipo_Movimientos {
+
+	int id_tipomovimiento;
+	String descripcion_tipomovimiento;
+	
+	
+	public Tipo_Movimientos() {}
+	
+	public Tipo_Movimientos(int id, String descripcion) {
+
+		id_tipomovimiento = id;
+		descripcion_tipomovimiento = descripcion;
+	}
+
+
+	public int getId_tipomovimiento() {
+		return id_tipomovimiento;
+	}
+
+
+	public void setId_tipomovimiento(int id_tipomovimiento) {
+		this.id_tipomovimiento = id_tipomovimiento;
+	}
+
+
+	public String getDescripcion_tipomovimiento() {
+		return descripcion_tipomovimiento;
+	}
+
+
+	public void setDescripcion_tipomovimiento(String descripcion_tipomovimiento) {
+		this.descripcion_tipomovimiento = descripcion_tipomovimiento;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Tipo_Movimientos [id_tipomovimiento=" + id_tipomovimiento + ", descripcion_tipomovimiento="
+				+ descripcion_tipomovimiento + "]";
+	}
+	
+
+}
